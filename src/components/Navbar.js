be603109236dcece84d8a2fee77f885794731c45
Yashit <veeryashit@gmail.com>
2022-03-28 20:23:34 +0530

    Init

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import reactLogo from "../images/logo192.png";
+
+export default function Navbar() {
+  return (
+    <nav>
+      <img src={reactLogo} alt="React Logo" />
+      <h3>ReactFacts</h3>
+      <h4>RedMonk Works</h4>
+    </nav>
+  );
+}
